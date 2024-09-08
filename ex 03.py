aa6f80488b48ed55bddcdfb3de8170d21956a21c
Henrique_Nardy <hnardyluz@gmail.com>
2024-09-08 13:56:13 -0300

    ex03
    #ex 3
    # Transponha uma matriz 3x3 (troque linhas por colunas).

--- ex 03.py
+++ ex 03.py
@@ -1,6 +1,5 @@
-# ex 02
-#Leia uma matriz 3x3 e imprima sua diagonal principal.
-
+#ex 3
+# Transponha uma matriz 3x3 (troque linhas por colunas).
 
 from random import randint
 
@@ -25,20 +24,26 @@ def imprime_matriz(m):
             print(f"| {m[i][j]} |", end="")
         print("")
 
-
-
-def imprime_diag_principal(m):
-    #considerando que a matriz será sempre quadrada
-
+def inverter_matriz(m):
     tam = len(m)
-    principal = list()
+    m_aux = [[0 for i in range(tam)] for i in range(tam)]
+
+    #clonar matriz para auxiliar
     for i in range(tam):
         for j in range(tam):
-            if i == j:
-                principal.append(m[i][j])
-    print(f'diagonal principal:{principal}')
+            m_aux[i][j] =m[i][j] # faz a inversão
+
+    #inverter
+    for i in range(tam):
+        for j in range(tam):
+            m[i][j] = m_aux[j][i]
+
+    return m
 
 
 mat = gerar_matriz_aleatoria()
 imprime_matriz(mat)
-imprime_diag_principal(mat)
+inv = inverter_matriz(mat)
+print("="*30)
+print("matriz invertida")
+imprime_matriz(inv)
